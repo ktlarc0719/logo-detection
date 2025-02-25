5a94b34f7ff12ff2042bfaeb7b6f4ee7726cd954
ktlarc0719 <ktlarc.0719@gmail.com>
2025-02-25 14:56:57 +0900

    commit

--- src/logo_detector.py
+++ src/logo_detector.py
@@ -290,8 +290,8 @@ def preprocess_image(img: np.ndarray) -> np.ndarray:
     return blurred
 
 async def fetch_image_data_from_api(limit: int = 30, sellerId: str = ''):
-    api_url = f"https://rex-server.f5.si/api/rex/inventory/logo-detection/get?limit={limit}&sellerId={sellerId}"
-    # api_url = f"http://localhost:3000/api/rex/inventory/logo-detection/get?limit={limit}&sellerId={sellerId}"
+    # api_url = f"https://rex-server.f5.si/api/rex/inventory/logo-detection/get?limit={limit}&sellerId={sellerId}"
+    api_url = f"http://localhost:3000/api/rex/inventory/logo-detection/get?limit={limit}&sellerId={sellerId}"
     
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url) as response:
@@ -330,7 +330,7 @@ def main():
             start_time = time.time()
             
             # APIかーを取得しース
-            image_data = asyncio.run(fetch_image_data_from_api(limit=100, sellerId=''))
+            image_data = asyncio.run(fetch_image_data_from_api(limit=100, sellerId='A18K0XW8117NAY'))
 
             # image_data = get_mock_image_data()
             
